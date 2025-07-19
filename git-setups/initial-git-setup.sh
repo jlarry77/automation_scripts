8df84b507b09edaa54b3fb87cbe3fea117833d91
jlarry77 <justynlarry@gmail.com>
2025-07-19 13:28:23 -0600

    Inital Script write for initial-git-setup.sh

--- git-setups/initial-git-setup.sh
+++ git-setups/initial-git-setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+###  The purpose of this script is to install and set up a git SSH Key and repository on an Ubuntu Machine.  If you already have connected
+###		your machine to github through ssh, there is an automation script called 'new-repo-setup.sh' that will walk you through
+###		setting up a new repository, that skips the ssh-keygen section.
+
+
 # - - - Error Handling Fucntion - - - #
 
 check_command(){
@@ -10,3 +15,56 @@ check_command(){
 }
 
 
+echo "Beginning Git Installation..."
+
+# 1.  Update System and install Git if not already installed:
+echo "Updating package lists..."
+sudo apt update
+check_command "apt update"
+
+echo "Installing Git..."
+sudo apt install git -y
+check_command "Git Installation"
+
+# 2.  Configure your Git username:
+read -p "Enter your Github User Name:  " git_username
+git config --global user.name "$git_username"
+echo "Git User Name set to:  $(git config --global user.name)"
+check_command "Set Username"
+
+# 3.  Configure your Git email address:
+read -p "Enter the email address you're using for Github:  " git_email
+git config --global user.email "$git_email"
+echo "Git e-mail set to: $(git config --global user.email)"
+check_command "Set e-mail"
+
+# 4.  Generate a new SSH Key Pair:
+echo "Setting a new SSH Key.  You can press 'Enter' to accept defaults..."
+ssh-keygen -t ed25519 -C "$git_email"
+check_command "Set SSH"
+
+# 5.  Display SSH Key for Github authorization:
+echo "Please copy the key below, and use it to pair in Github's Interface..."
+cat ~/.ssh/id_ed25519.pub
+read -p "Press Enter to continue..."
+
+# 6.  Start the SSH Agent:
+echo "Starting SSH Agent..."
+eval "$(ssh-agent -s)"
+check_command "Start SSH"
+
+# 7.  Add Private Key to SSH Agent:
+echo "Adding Private Key to SSH Agent..."
+ssh-add ~/.ssh/id_ed25519
+check_command "Add SSH"
+
+# 8.  Test Connection to github:
+echo "Checking Connection to Github..."
+ssh -T git@github.com
+check_command "Test Connection"
+
+# 9.  Clone Repository
+read -p "Please add the full URL to the Repository you would like to clone:  " git_repo
+git clone "$git_repo"
+echo "Git Repository set to: $(git clone)"
+check_command "Clone Repository"
